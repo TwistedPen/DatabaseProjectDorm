8e73dfe81455c4e606c65f8dff5f612784339fa7
Henriette Steenhoff <henriette_steenhoff@outlook.dk>
2015-04-09 15:55:06 +0200

    Started on the DML (not finished)
    Er gået lidt i gang -- kunne ikke vente #nemtKABSmøde

--- DML/DML.sql
+++ DML/DML.sql
@@ -1,3 +1,13 @@
 /**
 ** SQL DML
 **/
+
+delete from city;
+delete from dorm;
+delete from educational_instituion;
+delete from kitchen;
+delete from renting;
+delete from room;
+delete from signed_up;
+delete from student;
+insert into dorm values ();
