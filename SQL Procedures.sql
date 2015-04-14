cf330625d74db9b45ea7855c178f1f411da5bd9c
Henriette Steenhoff <henriette_steenhoff@outlook.dk>
2015-04-14 18:49:05 +0200

    Robustness

--- SQL Procedures.sql
+++ SQL Procedures.sql
@@ -6,9 +6,12 @@
 # Procedure for updating the amount to be paid as rent
 delimiter //
 
-create procedure update_rent(in room_size int(11), in new_rent decimal(8,2), dorm_name varchar(45))
+create procedure update_rent(in room_size int(11), in new_rent decimal(8,2), in dorm_name varchar(45))
 begin
-	insert into room_rent(Room_size, Monthly_fee, Dorm_name) values (room_size, new_rent, dorm_name);
+	IF dorm_name IS NOT NULL THEN
+	insert into room_rent(Room_size, Monthly_fee, Dorm_name) 
+    values (room_size, new_rent, dorm_name);
+    end IF;
 end; //
 
 /**
@@ -35,13 +38,17 @@ end; //
 # Procedure for moving internally in dorm
 create procedure move_internally(in old_room_id int(11), in new_room_id int(11))
 begin
-insert into room_rent(Room_size, Monthly_fee, Dorm_name) values (room_size, new_rent, dorm_name);
+insert into room_rent(Room_size, Monthly_fee, Dorm_name) 
+values (room_size, new_rent, dorm_name);
 end; //
 
 # Procedure for updating relocation date by room ID
 create procedure update_relocation_date(in room_id int(11), in new_relocation_date date)
 begin
-update renting set End_date = new_relcation_date where room_ID = room_id;
+	IF now() - new_relocation_date > 0 THEN
+	update renting set End_date = new_relcation_date
+	where room_ID = room_id;
+end IF;
 end;//
 /**
 ** Delete a studen from renting
