ecc1388a9a9e67268aa36e44dd47396752d18092
Thomas Malowanczyk <s103407@student.dtu.dk>
2015-04-14 19:10:54 +0200

    Added room rent function

--- SQL Functions.sql
+++ SQL Functions.sql
@@ -2,7 +2,7 @@
 ** Functions
 */
 delimiter //
-CREATE DEFINER=`root`@`localhost` FUNCTION `noOfRenters`(dormName varchar(45)) RETURNS int(11)
+CREATE FUNCTION `noOfRenters`(dormName varchar(45)) RETURNS int(11)
 BEGIN
 declare r_count integer;
 select count(*) into r_count from dorm
@@ -10,7 +10,7 @@ where D_name = dormName;
 RETURN r_count;
 END;//
 
-CREATE DEFINER=`root`@`localhost` FUNCTION `rentersAtUni`(uni varchar(45)) RETURNS int(11)
+CREATE FUNCTION `rentersAtUni`(uni varchar(45)) RETURNS int(11)
 BEGIN
 declare uni_count integer;
 select count(*) into uni_count from signed_up join educational_institution
@@ -26,6 +26,16 @@ into address from (room natural join kitchen) natural join dorm
 where ID = roomID;
 RETURN address;
 # missing concatenation of roomID to address
+END;//
+
+CREATE FUNCTION `getRoomRent`(roomID int(11)) RETURNS DECIMAL(8,2)
+BEGIN
+declare rent DECIMAL;
+select Monthly_fee into rent
+from room natural join kitchen natural join dorm natural join room_rent
+where room_ID = roomID;
+RETURN rent;
+#missing concatenation of roomID to address
 END;//
 
 delimiter ;	
