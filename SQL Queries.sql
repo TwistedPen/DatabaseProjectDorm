4d386a785626a10f417976ae6df0429e1300b86a
Henriette Steenhoff <henriette_steenhoff@outlook.dk>
2015-04-14 18:05:44 +0200

    edit to sign_ups-view

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -10,9 +10,9 @@ create view available_rooms as
 select * from room where room_ID not in (select room_ID from renting);
 
 /*
-* a table of all students signed up to the dorm rooms
+* a table of all students signed up to the dorm rooms ordered by distance to education
 */
 create view sign_ups as
-	select * from signed_up natural join student 
+	select First_name, Last_name, Dorm_name, Distance_to_edu from signed_up natural join student 
 	where CPR_no not in (select CPR_no from renting)
     order by signed_up.Distance_to_edu;
