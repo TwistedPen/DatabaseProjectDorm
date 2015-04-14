0cb2777767909bb81722b159d4a8dada7b81e0bd
Henriette Steenhoff <henriette_steenhoff@outlook.dk>
2015-04-14 13:49:39 +0200

    Added info to DML
    City, educational_institution, signed_up

--- DML/DML.sql
+++ DML/DML.sql
@@ -15,6 +15,10 @@ delete from student;
 
 
 # insert into tables
+insert into city values(2800,'Lyngby');
+insert into city values(2850,'Nærum');
+insert into city values(4000,'Roskilde');
+insert into city values(1165,'København');
 insert into dorm values('Kampsax Kollegiet','Kollegiebakken', '9', 2800);
 insert into dorm values('Paul Bergsø Kollegiet','Skodsborgvej', '190', 2850);
 insert into dorm values('Ostenfeld Kollegiet','Rævehøjvej', '36', 2800);
@@ -36,11 +40,13 @@ insert into room values(080001,000008,'801',12);
 insert into room values(080006,000008,'806',12);
 insert into room values(080009,000008,'809',12);
 insert into room values(080009,000008,'802',12);
+insert into educational_institution values('Danmark Tekniske Universitet','Anker Engelundsvej','1',2800);
+insert into educational_institution values('Københavns Universitet','Nørregade','10',1165);
+insert into educational_institution values('Roskilde Universitet','Universitetsvej','1',4000);
 insert into student values(2012902131,'Thomas','Malowanczyk',30316331,'Danmark Tekniske Universitet');
 insert into student values(2008882336,'Anne','Sloth',23506054,'Danmark Tekniske Universitet');
-insert into student values(2707923614,'Henriette','Steenhoff',26661248,'Danmark Tekniske Universitet');
+insert into student values(2702923614,'Henriette','Steenhoff',26661248,'Danmark Tekniske Universitet');
 insert into renting values();
+insert into signed_up values(2012902131,'Kampsax Kollegiet',020312,1.00,'xx','34',2800);
+insert into signed_up values(2702923614,'Ostenfeld Kollegiet',040415,5.00,'xx','25',2800);
 insert into signed_up values();
-insert into educational_institution values();
-insert into city values(2800,'Lyngby');
-insert into city values(2850,'Nærum');
