747dc668063aff2766ecc5051f8b0cb49faa52f3
Henriette Steenhoff <henriette_steenhoff@outlook.dk>
2015-04-14 18:59:16 +0200

    Added sign_ups-view

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -8,3 +8,11 @@
 */
 create view available_rooms as
 select * from room where room_ID not in (select room_ID from renting);
+
+/*
+* a table of all students signed up to the dorm rooms
+*/
+create view sign_ups as
+	select * from signed_up natural join student 
+	where CPR_no not in (select CPR_no from renting)
+    order by signed_up.Distance_to_edu;
