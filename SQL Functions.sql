5588a35bcefb2cc95c2c0fb51e23ab39a02303eb
Henriette Steenhoff <henriette_steenhoff@outlook.dk>
2015-04-14 18:01:54 +0200

    Functions

--- SQL Functions.sql
+++ SQL Functions.sql
@@ -14,18 +14,18 @@ CREATE DEFINER=`root`@`localhost` FUNCTION `rentersAtUni`(uni varchar(45)) RETUR
 BEGIN
 declare uni_count integer;
 select count(*) into uni_count from signed_up join educational_institution
-where Edu_name = uni group by Dorm_Name;
+where Edu_name = uni;
 RETURN uni_count;
 END;//
 
+ #drop function if exists getAddress;
  CREATE FUNCTION `getAddress`(roomID int(6)) RETURNS varchar(45)
 BEGIN
-declare address varchar(45);
-select distinct Street_name, Street_no 
-into address from (room natural join kitchen) natural join dorm 
-where ID = roomID;
-RETURN address;
-# missing concatenation of roomID to address
+declare roomNo varchar(45);
+select Street_no 
+into roomNo from (room natural join kitchen) natural join dorm 
+where Room_ID = roomID;
+RETURN concat(Street_name, Street_no, roomNo, substr(Room_ID,4));
 END;//
 
 delimiter ;	
