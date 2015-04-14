9759286148213a776a896ff361d558d9bce76b41
Thomas Malowanczyk <s103407@student.dtu.dk>
2015-04-14 22:19:41 +0200

    Added to_move_out view

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -6,13 +6,23 @@
 /*
 * a table of all the rooms with no renters
 */
+DROP VIEW IF EXISTS avaialbe_rooms;
 create view available_rooms as
-select * from room where room_ID not in (select room_ID from renting);
+	select * from room where room_ID not in (select room_ID from renting);
 
 /*
 * a table of all students signed up to the dorm rooms ordered by distance to education
 */
+DROP VIEW IF EXISTS sign_ups;
 create view sign_ups as
 	select First_name, Last_name, Dorm_name, Distance_to_edu from signed_up natural join student 
 	where CPR_no not in (select CPR_no from renting)
     order by signed_up.Distance_to_edu;
+    
+/*
+* a table of all students who are to move out within 30 days
+*/
+DROP VIEW IF EXISTS to_move_out;
+create view to_move_out as
+	select * from student natural join renting
+	where end_date is not null and end_date < ADDDATE(NOW(),30);
