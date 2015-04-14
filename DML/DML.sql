fc92593009fd20eb8aad4d65fb6501ce39b41523
Henriette Steenhoff <henriette_steenhoff@outlook.dk>
2015-04-14 13:08:57 +0200

    Deleted unneeded data from DML
    From merge conflict

--- DML/DML.sql
+++ DML/DML.sql
@@ -3,19 +3,6 @@
 **/
 
 <<<<<<< HEAD
-delete from city;
-delete from dorm;
-delete from educational_instituion;
-delete from kitchen;
-delete from renting;
-delete from room;
-delete from signed_up;
-delete from student;
-insert into dorm values ('Kampsax Kollegiet','Kollegiebakken 9','2800');
-insert into dorm values ('Nybrogårdkollegiet','Nybrovej 304','2800');
-insert into dorm values ('Prof. Ostenfeldt Kollegiet','xx','2800');
-insert into city values ('2800','Kgs. Lyngby');
-=======
 # clear tables
 delete from city;
 delete from dorm;
