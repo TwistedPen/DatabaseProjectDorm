f19cc3aaddad77fb30dd8cdb0bee6ebdc6fae450
Henriette Steenhoff <henriette_steenhoff@outlook.dk>
2015-04-14 18:38:22 +0200

    ..

--- SQL Procedures.sql
+++ SQL Procedures.sql
@@ -16,7 +16,7 @@ begin
 insert into room_rent(Room_size, Monthly_fee, Dorm_name) values (room_size, new_rent, dorm_name);
 end; //
 
-# Procedure for moving internally in dorm
+# Procedure for updating relocation date by room ID
 create procedure update_relocation_date(in room_id int(11), in new_relocation_date date)
 begin
 update renting set End_date = new_relcation_date where room_ID = room_id;
